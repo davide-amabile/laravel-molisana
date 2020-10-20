06caeba2abf6c33ade24be7bf501b44dfa506582
DAVIDE <davideamabile81gmail.com>
2020-10-20 15:08:44 +0200

    inclusione array

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
+
+Route::get('/', function () {
     return view('home');
-});
+})-> name('home');
+
+Route::get('/prodotti', function () {
+    return view('prodotti');
+})-> name('prodotti');
+
+Route::get('/news', function () {
+    return view('news');
+})-> name('news');
 
